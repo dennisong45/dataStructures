4fca82077972816f00f3636dd74d35749f4f58ac
unknown <STSC@LNVO-126965.ad.csueastbay.edu>
2020-04-14 21:25:10 -0700

    first version to show case data

---
+++ sketch.js
@@ -0,0 +1,23 @@
+let i = 0;
+let w = 10;
+function setup()
+{
+    createCanvas(800,200);
+    values = new Array(floor(width/w));
+    for (let i =0; i < values.length;i++)
+    {
+        values[i] = random(height);
+    } 
+    frameRate(5);
+}
+
+function draw()
+{
+    background(51);
+    for(let i =0; i <values.length;i++)
+    {
+        stroke(0);
+        fill(255);
+        rect(i*w,height - values[i],w,values[i]);
+    }
+}
